f189f702b089459426eebb018f23c1e582c3f4cc
Süleyman KENAR <suleyman@kenar.net>
2026-01-02 02:09:43 +0300

    updated standalone Plan class

--- src/StorableClasses/Plan.php
+++ src/StorableClasses/Plan.php
@@ -2,236 +2,60 @@
 
 namespace Iyzico\IyzipayLaravel\StorableClasses;
 
+use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Support\Arr;
 use Iyzico\IyzipayLaravel\Exceptions\Fields\PlanFieldsException;
 use Iyzico\IyzipayLaravel\ProductContract;
 use Iyzipay\Model\BasketItemType;
+use JsonSerializable;
 
-class Plan extends StorableClass implements ProductContract
+class Plan implements ProductContract, Arrayable, JsonSerializable
 {
+    public function __construct(
+        public string $id = '',
+        public string $name = '',
+        public float $price = 0.0,
+        public string $currency = 'TRY',
+        public string $interval = 'monthly',
+        public int $trialDays = 0,
+        public array $features = [],
+        public array $attributes = []
+    ) {
+        $this->validate();
+    }
 
-    /**
-     * The plan's id
-     *
-     * @var string
-     */
-    public string $id;
-
-    /**
-     * The plan's displayable name
-     *
-     * @var string
-     */
-    public string $name;
-
-    /**
-     * The plan's price.
-     *
-     * @var integer
-     */
-    public int $price = 0;
-
-    /**
-     * The plan's interval.
-     *
-     * @var string
-     */
-    public string $interval = 'monthly';
-
-    /**
-     * The number of trial days that come with the plan.
-     *
-     * @var int
-     */
-    public int $trialDays = 0;
-
-    /**
-     * The plan's features.
-     *
-     * @var array
-     */
-    public array $features = [];
-
-    /**
-     * The plan's attributes.
-     *
-     * @var array
-     */
-    public array $attributes = [];
-
-    /**
-     * The plan's currency
-     *
-     * @var string
-     */
-    public string $currency = 'TRY';
-
-    /**
-     * Set the name of the plan.
-     *
-     * @param string $name
-     *
-     * @return $this
-     */
-    public function name(string $name): static
+    private function validate(): void
     {
-        $this->name = $name;
+        if (empty($this->name)) {
+            throw new PlanFieldsException('Plan name cannot be blank!');
+        }
 
-        return $this;
+        if (empty($this->id)) {
+            throw new PlanFieldsException('Plan ID cannot be blank!');
+        }
+    }
+
+    public function __toString(): string
+    {
+        return json_encode($this);
     }
 
     /**
-     * Set the id of the plan.
-     *
-     * @param string $id
-     *
-     * @return $this
+     * Create instance from array (Helper for config/JSON hydration).
+     * @throws \Exception
      */
-    public function id(string $id): static
+    public static function fromArray(array $attributes): self
     {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    /**
-     * Set the price of the plan.
-     *
-     * @param integer $price
-     *
-     * @return $this
-     */
-    public function price(int $price): static
-    {
-        $this->price = $price;
-
-        return $this;
-    }
-
-    /**
-     * Specify that the plan is on a yearly interval.
-     *
-     * @return $this
-     */
-    public function yearly(): static
-    {
-        $this->interval = 'yearly';
-
-        return $this;
-    }
-
-    /**
-     * Specify the number of trial days that come with the plan.
-     *
-     * @param int $trialDays
-     *
-     * @return $this
-     */
-    public function trialDays(int $trialDays): static
-    {
-        $this->trialDays = $trialDays;
-
-        return $this;
-    }
-
-    /**
-     * Specify the currency of plan.
-     *
-     * @param $currency
-     *
-     * @return $this
-     */
-    public function currency($currency): static
-    {
-        $this->currency = $currency;
-
-        return $this;
-    }
-
-    /**
-     * Specify the plan's features.
-     *
-     * @param  array $features
-     *
-     * @return $this
-     */
-    public function features(array $features): static
-    {
-        $this->features = $features;
-
-        return $this;
-    }
-
-    /**
-     * Get a given attribute from the plan.
-     *
-     * @param  string $key
-     * @param  mixed $default
-     *
-     * @return mixed
-     */
-    public function attribute($key, $default = null): mixed
-    {
-        return Arr::get($this->attributes, $key, $default);
-    }
-
-    /**
-     * Specify the plan's attributes.
-     *
-     * @param  array $attributes
-     *
-     * @return $this
-     */
-    public function attributes(array $attributes): static
-    {
-        $this->attributes = array_merge($this->attributes, $attributes);
-
-        return $this;
-    }
-
-
-    protected function getFieldExceptionClass(): string
-    {
-        return PlanFieldsException::class;
-    }
-
-    public function getKey(): string
-    {
-        return $this->name;
-    }
-
-    public function getKeyName(): string
-    {
-        return 'name';
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function getPrice(): int
-    {
-        return $this->price;
-    }
-
-    public function getCategory(): string
-    {
-        return 'Plan';
-    }
-
-    public function getType(): string
-    {
-        return BasketItemType::VIRTUAL;
-    }
-
-    public function toArray(): array
-    {
-        return [
-            'name' => $this->name,
-            'price' => $this->price,
-            'currency' => $this->currency
-        ];
+        return new self(
+            id:         $attributes['id'] ?? '',
+            name:       $attributes['name'] ?? '',
+            price:      (float) ($attributes['price'] ?? 0),
+            currency:   $attributes['currency'] ?? 'TRY',
+            interval:   $attributes['interval'] ?? 'monthly',
+            trialDays:  (int) ($attributes['trialDays'] ?? 0),
+            features:   $attributes['features'] ?? [],
+            attributes: $attributes['attributes'] ?? []
+        );
     }
 
     /**
@@ -249,6 +73,122 @@ class Plan extends StorableClass implements ProductContract
             throw new \Exception("Subscription plan with key [{$key}] not found in iyzipay configuration.");
         }
 
-        return new static($config);
+        // Inject the Key as the ID, since it's the array key in config
+        $config['id'] = $key;
+
+        return self::fromArray($config);
+    }
+
+    public function name(string $name): static
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function id(string $id): static
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function price(float $price): static
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    public function currency(string $currency): static
+    {
+        $this->currency = $currency;
+        return $this;
+    }
+
+    public function yearly(): static
+    {
+        $this->interval = 'yearly';
+        return $this;
+    }
+
+    public function monthly(): static
+    {
+        $this->interval = 'monthly';
+        return $this;
+    }
+
+    public function trialDays(int $trialDays): static
+    {
+        $this->trialDays = $trialDays;
+        return $this;
+    }
+
+    public function features(array $features): static
+    {
+        $this->features = $features;
+        return $this;
+    }
+
+    public function attributes(array $attributes): static
+    {
+        $this->attributes = array_merge($this->attributes, $attributes);
+        return $this;
+    }
+
+    public function attribute(string $key, mixed $default = null): mixed
+    {
+        return Arr::get($this->attributes, $key, $default);
+    }
+
+    public function getKey(): string
+    {
+        return $this->id;
+    }
+
+    public function getKeyName(): string
+    {
+        return 'id';
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function getCategory(): string
+    {
+        return 'Plan';
+    }
+
+    public function getType(): string
+    {
+        return BasketItemType::VIRTUAL;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id'        => $this->id,
+            'name'      => $this->name,
+            'price'     => $this->price,
+            'currency'  => $this->currency,
+            'interval'  => $this->interval,
+            'trialDays' => $this->trialDays,
+            'features'  => $this->features,
+            'attributes'=> $this->attributes,
+        ];
+    }
+
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
+    }
+
+    protected function getFieldExceptionClass(): string
+    {
+        return PlanFieldsException::class;
     }
 }
